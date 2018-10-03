7f4dfa571283b0da8367b0ec06f1cf8956b33872
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-10-03 18:02:40 +0200

    vue

---
+++ vue-tasks/src/components/Tasks.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li>tasca1</li>
+        <li>tasca2</li>
+        <li>tasca3</li>
+        <li>tasc4</li>
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
