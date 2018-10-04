b6affc2850e037b5b6f4f77f5afd6b34b86ea2d8
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-10-04 19:47:12 +0200

    sync

--- vue-tasks/src/components/Tasks.vue
+++ vue-tasks/src/components/Tasks.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <input type="text" v-model="newTask">
+        <input type="text"
+               v-model="newTask" @keyup.enter="add">
+        <button @click="add">Afegir</button>
         <!--{{ newTask}} = echo -->
         <!--<input :value="newTask" @input="newTask = $event.target.value">-->
 
         <ul>
-            <li v-for="task in tasks"
-                v-if="task.completed"><strike>{{task.name}}</strike></li>
-            <li v-else>{{task.name}}</li>
+            <!--<li v-for="task in tasks"-->
+            <!--v-if="task.completed"><strike>{{task.name}}</strike></li>-->
+            <!--<li v-else>{{task.name}}</li>-->
+
+            <li v-for="task in tasks" :class="{strike: task.completed}">{{task.name}}</li>
         </ul>
     </div>
 </template>
 
 <script>
-
     //document.getEkebementById
     export default {
         data(){
@@ -23,24 +26,22 @@
                     {
                         name:'Comprar pa ',
                         completed:false
-
                     },
                     {
                         name:'Comprar llet ',
                         completed:true
-
                     },
                     {
                         name:'Estudiar php ',
                         completed:false
-
                     },
                 ]
             }
         },
         methods:{
-            xivato(){
-                console.log('sdasd')
+            add(){
+                // console.log('TODO ADD')
+                this.tasks.splice(0,0,{name:this.newTask,completed:false})
             }
         }
     }
@@ -55,5 +56,15 @@
 </script>
 
 <style>
-
+    .strike{
+        text-decoration: line-through;
+    }
 </style>
+    // {
+    //     marca:'Renault',
+    //     consum:'5l/100',
+    //     start: function (){
+    //         console.log('arraca');
+    // }
+    //
+    // }
