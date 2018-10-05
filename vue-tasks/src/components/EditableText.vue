c70d96c29ff82820060490985172daca07bc4924
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-10-05 17:09:42 +0200

    task_lapiz_edit

--- vue-tasks/src/components/EditableText.vue
+++ vue-tasks/src/components/EditableText.vue
@@ -2,10 +2,13 @@
     <span>
         <span v-if="!editing" @dblclick="editing=true">
             {{currentText}}
+            <svg class="h-3 w-3" @click="editing=true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+
         </span>
         <span v-if="editing" @keyup.esc="editing=false"
               @keyup.enter="edit">
             <input type="text" v-model="currentText">
+
         </span>
     </span>
 </template>
