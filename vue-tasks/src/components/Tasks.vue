a6c69a49f89504c61e571964e67ec9a9f130b4a8
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-10-05 15:16:59 +0200

    filtres_tasks

--- vue-tasks/src/components/Tasks.vue
+++ vue-tasks/src/components/Tasks.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
         <div class="flex flex-col">
-        <h1 class="text-center text-red-light">Tasques</h1>
+        <h1 class="text-center text-red-light">Tasques ({{total}}): </h1>
             <div class="flex-row"  >
 
         <input type="text" placeholder="Nova Tasca"
@@ -18,14 +18,15 @@
         </ul>
 
 <br>
-            <h3>FILTRES</h3>
+            <h3>Filtros:</h3>
             <br>
-            <!--Activa filter:{{filter}}-->
-            <ul>
-                <li><button @click="setfilter=('all')">Totes</button></li>
-                <li><button @click="setfilter=('completed')">Completades</button></li>
-                <li><button @click="setfilter=('active')">Pendets</button></li>
+            Filtre emprat -> {{ filter }}
 
+            <ul>
+                <br>
+                <li><button @click="setFilter('all')">Totes</button></li>
+                <li><button @click="setFilter('completed')">Completades</button></li>
+                <li><button @click="setFilter('active')">Pendents</button></li>
             </ul>
         </div>
     </div>
@@ -51,52 +52,49 @@
     }
     //document.getEkebementById
     export default {
-        data(){
+        data() {
             return {
-                filter:'all',
-                newTask:'',
+                filter: 'all', // All Completed Active
+                newTask: '',
                 tasks: [
                     {
-                        id:1,
-                        name:'Comprar pa ',
-                        completed:false
+                        id: 1,
+                        name: 'Comprar pa',
+                        completed: false
                     },
                     {
-                        id:2,
-                        name:'Comprar llet ',
-                        completed:true
+                        id: 2,
+                        name: 'Comprar llet',
+                        completed: false
                     },
                     {
-                        id:3,
-                        name:'Estudiar php ',
-                        completed:false
-                    },
+                        id: 3,
+                        name: 'Estudiar PHP',
+                        completed: true
+                    }
                 ]
             }
         },
         computed: {
-            total(){
-              return this.tasks.length
+            total() {
+                return this.tasks.length
             },
-            filteredTasks(){
-                //Segons el filtre actiu
+            filteredTasks() {
+                // Segons el filtre actiu
+                // Alternativa switch/case -> array associatiu
                 return filters[this.filter](this.tasks)
             }
         },
-        methods:{
-            setfilter(newFilter){
-
-                this.filter=newFilter
-
+        methods: {
+            setFilter(newFilter) {
+                this.filter = newFilter
             },
-            add(){
-                // console.log('TODO ADD')
-                this.tasks.splice(0,0,{name:this.newTask,completed:false})
+            add() {
+                this.tasks.splice(0,0,{ name: this.newTask, completed: false } )
                 this.newTask=''
             },
-            remove(task){
+            remove(task) {
                 window.console.log(task)
-
                 this.tasks.splice(this.tasks.indexOf(task),1)
             }
         }
@@ -112,7 +110,7 @@
 </script>
 
 <style>
-    .strike{
+    .strike {
         text-decoration: line-through;
     }
 
