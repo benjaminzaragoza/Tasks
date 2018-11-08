0f4da9f17844428d083288bfa211f472c2cc60aa
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-11-08 19:17:00 +0100

    register

--- resources/js/components/RegisterForm.vue
+++ resources/js/components/RegisterForm.vue
@@ -1,13 +1,14 @@
 <template>
     <v-form action="/register" method="post">
         <v-toolbar dark color="primary">
-            <v-toolbar-title>Register Form</v-toolbar-title>
+            <v-toolbar-title>Formulari de Registre</v-toolbar-title>
             <v-spacer></v-spacer>
         </v-toolbar>
         <v-card-text>
+
             <input type="hidden" name="_token" :value="csrfToken">
-            <v-text-field prepend-icon="person" name="name" label="name" type="text" v-model="name" :error-messages="nameErrors" @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
-            <v-text-field prepend-icon="person" name="email" label="email" type="text" v-model="dataEmail" :error-messages="emailErrors" @input="$v.dataEmail.$touch()" @blur="$v.dataEmail.$touch()"></v-text-field>
+            <v-text-field prepend-icon="person" name="name" label="Nom" type="text" v-model="name" :error-messages="nameErrors" @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
+            <v-text-field prepend-icon="person" name="email" label="E-mail" type="text" v-model="dataEmail" :error-messages="emailErrors" @input="$v.dataEmail.$touch()" @blur="$v.dataEmail.$touch()"></v-text-field>
             <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" v-model="password" :error-messages="passwordErrors" @input="$v.password.$touch()" @blur="$v.password.$touch()"></v-text-field>
             <v-text-field id="password_confirmation" prepend-icon="lock" name="password_confirmation" label="Password Confirmation" type="password" v-model="password_confirmation" :error-messages="password_confirmationErrors" @input="$v.password_confirmation.$touch()" @blur="$v.password_confirmation.$touch()"></v-text-field>
 
@@ -18,9 +19,7 @@
         </v-card-actions>
     </v-form>
 </template>
-
 <script>
-// validationMixin = vuelidate.validationMixin
 import { validationMixin } from 'vuelidate'
 import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 export default {
@@ -48,29 +47,29 @@ export default {
     emailErrors () {
       const errors = []
       if (!this.$v.dataEmail.$dirty) return errors
-      !this.$v.dataEmail.minLength && errors.push('El camp email ha de tindre una mida minima de 6 caracters')
-      !this.$v.dataEmail.required && errors.push('El camp email es obligatori')
-      !this.$v.dataEmail.email && errors.push('El camp email ha de tindre un format mail valid')
+      !this.$v.dataEmail.minLength && errors.push('El camp e-mail ha de tindre una mida mínima de 6 caràcters')
+      !this.$v.dataEmail.required && errors.push('El camp e-mail és obligatori')
+      !this.$v.dataEmail.email && errors.push('El camp e-mail ha de tindre un format mail vàlid')
       return errors
     },
     nameErrors () {
       const errors = []
       if (!this.$v.name.$dirty) return errors
-      !this.$v.name.minLength && errors.push('El camp name ha de tindre una mida minima de 6 caracters')
-      !this.$v.name.required && errors.push('El camp name es obligatori')
+      !this.$v.name.minLength && errors.push('El camp nom ha de tindre una mida mínima de 6 caràcters')
+      !this.$v.name.required && errors.push('El camp nom és obligatori')
       return errors
     },
     passwordErrors () {
       const errors = []
       if (!this.$v.password.$dirty) return errors
-      !this.$v.password.minLength && errors.push('El camp password ha de tindre una mida minima de 6 caracters')
-      !this.$v.password.required && errors.push('El camp password es obligatori')
+      !this.$v.password.minLength && errors.push('El camp password ha de tindre una mida mínima de 6 caràcters')
+      !this.$v.password.required && errors.push('El camp password és obligatori')
       return errors
     },
     password_confirmationErrors () {
       const errors = []
       if (!this.$v.password_confirmation.$dirty) return errors
-      !this.$v.password_confirmation.sameAsPassword && errors.push('Les contrassenyes no coincideixen!')
+      !this.$v.password_confirmation.sameAsPassword && errors.push('Les contrasenyes no coincideixen')
       return errors
     }
   }
