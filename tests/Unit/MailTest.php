b05f0af5d70336843f3aedcf81e3ab8797989afb
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2018-12-03 21:19:55 +0100

    webmail

--- tests/Unit/MailTest.php
+++ tests/Unit/MailTest.php
@@ -1,28 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: benjamin
- * Date: 3/12/18
- * Time: 19:54
- */
-
 namespace Tests\Unit;
+use App\Mail\TestDinamicEmail;
+use App\Mail\TestEmail;
+use App\Mail\TestTextEmail;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
-
 class MailTest extends TestCase
 {
     use RefreshDatabase;
     /**
      * @test
      */
-    public function send_email()
+    public function send_markdown_email()
     {
-        //1
         $user = factory(User::class)->create();
-        //2
         Mail::to($user)->send(new TestEmail());
+        $this->assertTrue(true);
+    }
+    /**
+     * @test
+     */
+    public function send_email_text()
+    {
+        $user = factory(User::class)->create();
+        Mail::to($user)->send(new TestTextEmail());
+        $this->assertTrue(true);
+    }
+    /**
+     * @test
+     */
+    public function send_dinamic_email_text()
+    {
+        $user = factory(User::class)->create();
+        Mail::to($user)->send(new TestDinamicEmail($user));
+        $this->assertTrue(true);
     }
 }
