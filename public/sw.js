65c57f5668ca0fca3973e915e4a040c2f4e5e13e
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2019-03-07 16:39:23 +0100

    a

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.a541c6c90bd4cb89829b23be52fab25f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.d36c52b586eb1c7fc2eb09e1eca6492e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // importScripts('/service-worker/precache-manifest.fc21ecfebb4853d725aa822e2382fb14.js', 'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js')
 
