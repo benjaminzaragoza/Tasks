cac8eadee90528c99fe6e25ed68ecc7a8955fd32
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2019-03-19 17:25:10 +0100

    Sync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -160,7 +160,7 @@
             </section>
 
             <section>
-                <v-parallax-webp src="img/adeu.webp" height="380" class="dos" alt-format="jpg">
+                <v-parallax-webp src="img/adeu.webp" height="380"  alt-format="jpg">
                     <v-layout column align-center justify-center>
                         {{--<v-btn large color="primary"--}}
                                {{--href="/home"--}}
