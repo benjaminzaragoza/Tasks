800aef45e693ab689714dd3627ebc059db6b3a96
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2019-05-21 19:05:57 +0200

    Sync

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
-
+//ada
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
